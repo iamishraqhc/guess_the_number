619d22e6bc6aa22ca25103e13e235fed8b8b1b99
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-07-28 23:32:10 +0200

    renamed user to guess variable

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,14 +3,14 @@ import random
 number = random.randint(1,10)
 tries = 3
 for i in range(0,tries):
-    user = int(input("Guess the number: "))
-    if user == number:
+    guess = int(input("Guess the number: "))
+    if guess == number:
         print("Awesome!!")
         print(f"You have guessed the number right. It's {number}")
         break
     else:
         tries = tries - 1
         print(f"Tries left:  {tries}")
-if user != number:
+if guess != number:
     print(f"Sorry, your guesses were incorrect. The number is {number}")
     
